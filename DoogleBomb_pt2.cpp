d003b739cac890e9184834f50f8b46933c9740df
Doogle876 <dallen8763@gmail.com>
2017-04-05 19:58:11 -0400

    Code upload

---
+++ DoogleBomb_pt2.cpp
@@ -0,0 +1,54 @@
+// ConsoleApplication4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char x = y;
+string Wires = "W", "R", "B", "O", "G", "P";
+char b = 0;
+cout << "You are running out of time, Please defuse the bomb" << endl << "Enter the appropriate sequence of wires to defuse the bomb";
+cin >> Wires;
+int main()
+
+		for (y= 0; y<Wires.atcurrent(); y++)
+		{
+			{
+			case W: if W(next == "W"|| next == "B")
+				break;
+
+			case R: 
+				if (next != "G")
+				break;
+
+			case B: 
+				if (next == "G" || next == "W" || next == "O") 
+				break;
+
+			case O: 
+				if (next != "R" || next != "B") 
+				break;
+
+			case G: 
+				if (next != "O" && next != "W")
+				break;
+
+			case P: 
+				if (next == "P" || next == "G"|| next == "O" || next == "W") 
+				break;
+			}
+		if
+		{
+			b = 0;
+			cout << "Boom Bye Bye Yuh Likkle Bombo" << endl << "Battybwoy Yuh Kill We";
+		}
+		else
+		{
+			cout << "Bomb is Defused" << endl << "You saved our lives, You are the GOAT";
+		}
+
+	}
+
