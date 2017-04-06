3e5ba9cf919e50e5772d778f4b3c058d7363f551
Doogle876 <dallen8763@gmail.com>
2017-04-05 21:09:05 -0400

    mod

--- DoogleBomb_pt2.cpp
+++ DoogleBomb_pt2.cpp
@@ -7,48 +7,50 @@
 
 using namespace std;
 
-char x = y;
-string Wires = "W", "R", "B", "O", "G", "P";
-char b = 0;
-cout << "You are running out of time, Please defuse the bomb" << endl << "Enter the appropriate sequence of wires to defuse the bomb";
-cin >> Wires;
+
 int main()
-
-		for (y= 0; y<Wires.atcurrent(); y++)
+{
+	char b = '0';
+	char wire[] = "W" "R" "B" "O" "G" "P";
+	cout << "You are running out of time, Please defuse the bomb" << endl << "Enter the appropriate sequence of wires to defuse the bomb";
+	cin >> wire;
+	while (char wire[] = "W" "R" "B" "O" "G" "P")
+	{
+		string str(wire);
+	}
+	for (int y = 0; y <wire.atcurrent(); y++)
+		/*{
 		{
-			{
-			case W: if W(next == "W"|| next == "B")
-				break;
+		case W: if W(next == "W"|| next == "B")
+		break;
 
-			case R: 
-				if (next != "G")
-				break;
+		case R:
+		if (next != "G")
+		break;
 
-			case B: 
-				if (next == "G" || next == "W" || next == "O") 
-				break;
+		case B:
+		if (next == "G" || next == "W" || next == "O")
+		break;
 
-			case O: 
-				if (next != "R" || next != "B") 
-				break;
+		case O:
+		if (next != "R" || next != "B")
+		break;
 
-			case G: 
-				if (next != "O" && next != "W")
-				break;
+		case G:
+		if (next != "O" && next != "W")
+		break;
 
-			case P: 
-				if (next == "P" || next == "G"|| next == "O" || next == "W") 
-				break;
-			}
+		case P:
+		if (next == "P" || next == "G"|| next == "O" || next == "W")
+		break;
+		}
 		if
 		{
-			b = 0;
-			cout << "Boom Bye Bye Yuh Likkle Bombo" << endl << "Battybwoy Yuh Kill We";
+		b = 0;
+		cout << "Boom Bye Bye Yuh Likkle Bombo" << endl << "Battybwoy Yuh Kill We";
 		}
 		else
 		{
-			cout << "Bomb is Defused" << endl << "You saved our lives, You are the GOAT";
+		cout << "Bomb is Defused" << endl << "You saved our lives, You are the GOAT";
 		}
-
-	}
-
+		*/
