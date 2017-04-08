925f9d05efcfcf7587637ae1ee24874a8b357b42
Doogle876 <dallen8763@gmail.com>
2017-04-08 10:28:28 -0400

    Question #1
    Displaying Name and Address

---
+++ NameandAddress.cpp
@@ -0,0 +1,16 @@
+// NameandAddress.cpp : Defines the entry point for the console application.
+//This is a C++ program that displays my name and address.
+
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "My name and Address is:" " Douglas Allen, 123 Sesame Street, Henrico VA, 12345" << endl;
+
+	return 0;
+}
